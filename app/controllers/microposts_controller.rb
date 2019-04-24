dee3cb1a68d21ea28f833e0df0fea557c9cb1e31
Alexis80 <adiea1@tcnj.edu>
2019-04-24 19:32:41 -0400

    Update microposts_controller.rb

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,7 +8,7 @@ class MicropostsController < ApplicationController
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
-      redirect_to root_url
+      redirect_to users_show_path(:id => current_user.id)
     else
       @feed_items = []
       render 'static_pages/home'
