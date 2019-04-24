0ae69b7b141b598c5016eda0bec3fa3c6e6c1ddb
Alexis80 <adiea1@tcnj.edu>
2019-04-24 18:56:22 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+   #from railstutorial.org on 4/18/19
+        protect_from_forgery with: :exception
+        include SessionsHelper
+        rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+  def render_404
+    render :template => "errors/error_404", :status => 404
+  end
+  private
+
+  def logged_in_user
+      unless logged_in?
+
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
