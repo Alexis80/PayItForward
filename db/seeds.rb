904c0ff42a3625e532d3a7d4b881ffea7a616b7a
Alexis80 <adiea1@tcnj.edu>
2019-04-24 21:33:26 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+# This file should contain all the record creation needed to seed the database with its default values.i
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
+# From railstutorial.org on 4/19/19
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "Example Admin",
+             email: "example@ex.org",
+             address: "123 My Avenue",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  address = "576 Faker Street"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               address: address,
+               password:              password,
+               password_confirmation: password)
+end
